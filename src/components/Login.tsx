7a798b66ff70f59addd6a814ff2bdcdd538b2e07
ffdietz <felipefierrod@gmail.com>
2022-11-07 20:45:41 +0100

    auth form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,44 @@
-import { Box, Button, Container } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Box, Button, Container, FormControl, FormLabel, Input, Textarea } from '@chakra-ui/react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+
+interface Auth{
+  user: string;
+  pass: string;
+}
 
 function Login() {
   const [activeLogin, setActiveLogin] = useState<boolean>(false);
+  const [auth, setComment] = useState<Auth>({
+    user: '',
+    pass: '',
+  });
 
-  const handleAuth = (
+  const handleUserInput = (event: ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+    setComment((values) => ({
+      ...values,
+      author: event.target.value,
+    }));
+  };
+
+  const handlePasswordInput = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    event.persist();
+    setComment((values) => ({
+      ...values,
+      comment: event.target.value,
+    }));
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // if (id) {
+    //   const response = await postComment(id, comment);
+    //   console.log(response);
+    // }
+  };
+
+  const handleActiveAuth = (
     event: React.MouseEvent<HTMLButtonElement | MouseEvent>
   ) => {
     event.preventDefault();
@@ -15,15 +49,36 @@ function Login() {
   return (
     <Container marginTop="5vh">
       <>
-        <Button onClick={handleAuth}>
-          Login
-        </Button>
-        {activeLogin &&
-        <Box>Active</Box>
-        }
+        <Button onClick={handleActiveAuth}>Login</Button>
+        {activeLogin && (
+          <Box>
+            <form onSubmit={handleSubmit}>
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  type="text"
+                  name="comment[author]"
+                  defaultValue={auth.user}
+                  onChange={handleUserInput}
+                  placeholder="Your name"
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Comment</FormLabel>
+                <Textarea
+                  name="comment[comment]"
+                  defaultValue={auth.pass}
+                  onChange={handlePasswordInput}
+                  placeholder="Your comment"
+                />
+              </FormControl>
+              <Button type="submit">Submit</Button>
+            </form>
+          </Box>
+        )}
       </>
     </Container>
-  )
+  );
 }
 
 export default Login
